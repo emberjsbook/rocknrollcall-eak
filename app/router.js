434f26b7eac330b7ee6dc9e4d15ed0e6d5c3def1
jessecravens <jesse.cravens@gmail.com>
2014-05-06 23:44:39 -0500

    Chapter 8: Update Router and Create an ActivitiesRoute.

--- app/router.js
+++ app/router.js
@@ -3,9 +3,9 @@ var Router = Ember.Router.extend(); // ensure we don't share routes between all 
 Router.map(function() {
   this.route('component-test');
   this.route('helper-test');
-  // this.resource('posts', function() {
-  //   this.route('new');
-  // });
+  this.resource('activities', {
+    path: '/activities'
+  });
 });
 
 export default Router;
