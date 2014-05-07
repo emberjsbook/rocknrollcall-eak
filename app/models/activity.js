3bc41809e7fdeaf26ba7fcb35075e465c827197c
jessecravens <jesse.cravens@gmail.com>
2014-05-06 23:42:13 -0500

    Chapter 8: Create an Activity model.

---
+++ app/models/activity.js
@@ -0,0 +1,9 @@
+var Activity = DS.Model.extend({
+  display_id: DS.attr('string'),
+  type: DS.attr('string'),
+  display_name: DS.attr('string'),
+  hotttnesss: DS.attr('number'),
+  timestamp: DS.attr()
+});
+
+export default Activity;
