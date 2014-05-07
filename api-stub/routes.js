3e49a4d812547e64f760d0247f1898946d304e1b
jessecravens <jesse.cravens@gmail.com>
2014-05-06 23:39:37 -0500

    Chapter 8: Add the Express.js activities endpoint and fixture data.

--- api-stub/routes.js
+++ api-stub/routes.js
@@ -1,29 +1,30 @@
 module.exports = function(server) {
 
   // Create an API namespace, so that the root does not
-  // have to be repeated for each end point.
-  server.namespace('/api', function() {
+	// have to be repeated for each end point.
+	server.namespace("/api", function() {
 
-    // Return fixture data for '/api/posts/:id'
-    server.get('/posts/:id', function(req, res) {
-      var post = {
-        "post": {
-          "id": 1,
-          "title": "Rails is omakase",
-          "comments": ["1", "2"],
-          "user" : "dhh"
-        },
-
-        "comments": [{
-          "id": "1",
-          "body": "Rails is unagi"
-        }, {
-          "id": "2",
-          "body": "Omakase O_o"
-        }]
-      };
-
-      res.send(post);
-    });
-  });
+		// Return fixture data for "/api/activities"
+		server.get("/activities", function(req, res) {
+			var activities =
+			{
+				"activities": [{
+					id: 0,
+					display_id: "Activity1",
+					searchresults_type: "song",
+					display_name: "On the Road Again",
+					hotttnesss: 54,
+					timestamp: "Fri Dec 06 2013 01:05:53 GMT-0600 (CST)"
+				}, {
+					id: 1,
+					display_id: "Activity2",
+					searchresults_type: "artist",
+					display_name: "Willie Nelson",
+					hotttnesss: 99,
+					timestamp: "Fri Dec 06 2013 01:05:53 GMT-0600 (CST)"
+				}]
+			};
+			res.send(activities);
+		});
+	});
 };
