e8c083e1aaacf8a7986482645931d6f5d9682f7a
jessecravens <jesse.cravens@gmail.com>
2014-05-21 22:52:03 -0500

    Chapter 10: Unit-ActivitiesRoute - isolate the Route's model hook by creating a simple mock store and defining a find() method.

--- tests/unit/routes/activities-test.js
+++ tests/unit/routes/activities-test.js
@@ -8,3 +8,35 @@ test("it exists", function(){
   ok(this.subject());
   ok(this.subject() instanceof Activities);
 });
+
+test("#model", function(){
+
+  var store = {
+    find: function() {
+      return [{
+          id: 0,
+          display_id: 'Activity1',
+          type: 'song',
+          display_name: 'On The Road Again',
+          hotttnesss: 54,
+          timestamp: 'Fri Dec 06 2013 01:05:53 GMT-0600 (CST)'
+        }
+      ];
+    }
+  };
+
+  var route = this.subject();
+
+  route.set('store', store);
+
+  deepEqual(route.model(), [{
+      id: 0,
+      display_id: 'Activity1',
+      type: 'song',
+      display_name: 'On The Road Again',
+      hotttnesss: 54,
+      timestamp: 'Fri Dec 06 2013 01:05:53 GMT-0600 (CST)'
+    }
+  ]);
+
+});
